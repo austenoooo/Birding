7f0cec4dab2227e0a1311b733c46f9873f0b3587
Austenoooo <yixuan.li683@dukekunshan.edu.cn>
2023-04-22 12:30:51 -0400

    created files

---
+++ script.js
@@ -0,0 +1,17 @@
+import * as THREE from "three";
+
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+
+let scene, camera, renderer;
+let controls;
+
+function init() {
+
+}
+
+function loop() {
+
+}
+
+init();
